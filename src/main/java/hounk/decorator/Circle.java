2736407a656b1fcf4e796849804258b7337ede09
naikuo <naikuoh@hupofintech.com>
2019-09-25 21:58:16 +0800

    commit

---
+++ src/main/java/hounk/decorator/Circle.java
@@ -0,0 +1,14 @@
+package hounk.decorator;
+
+/**
+ * @Description TODO
+ * @Author naikuo
+ * @Date 2019/9/25 9:51 PM
+ */
+public class Circle implements Shape {
+
+    public void draw() {
+        System.out.println("this is Circle");
+    }
+
+}
---
+++ src/main/java/hounk/decorator/Shape.java
@@ -0,0 +1,12 @@
+package hounk.decorator;
+
+/**
+ * @Description 装饰器模式
+ * @Author naikuo
+ * @Date 2019/9/25 9:49 PM
+ */
+public interface Shape {
+
+    void draw();
+
+}
