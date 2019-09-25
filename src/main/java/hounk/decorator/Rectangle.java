b29e505aa55f7259f808c3532ae2e2c352274dd8
naikuo <naikuoh@hupofintech.com>
2019-09-25 22:01:02 +0800

    commit

---
+++ src/main/java/hounk/decorator/Rectangle.java
@@ -0,0 +1,12 @@
+package hounk.decorator;
+
+/**
+ * @Description TODO
+ * @Author naikuo
+ * @Date 2019/9/25 10:00 PM
+ */
+public class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("this is rechangle");
+    }
+}
